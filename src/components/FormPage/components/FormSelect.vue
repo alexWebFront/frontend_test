f08e23e8c4fb39df3aa1a16af523c449143ac4dd
Alexey <skejing@yandex.ru>
2025-02-04 20:23:03 +0400

    Создал проект, убрал лишнее, подготовил структуру компонентов

---
+++ src/components/FormPage/components/FormSelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="$style.SelectWrapper">
+    <select
+      :class="$style.formSelect"
+    >
+      <option value="" disabled>{{ label }}</option>
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormSelect',
+
+  props: {
+    options: {
+      type: Array,
+      default: () => []
+    },
+
+    label: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+.SelectWrapper {
+  //
+}
+
+.formSelect {
+  //
+}
+</style>
---
+++ src/views/FormPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="$style.PageWrapper">
+    <form :class="$style.form">
+      <div :class="$style.formContainer">
+        <FormSelect/>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+// Components
+import FormSelect from '@/components/FormPage/components/FormSelect.vue'
+
+export default {
+  name: 'FormPage',
+
+  components: { FormSelect }
+}
+</script>
+
+<style lang="scss" module>
+.PageWrapper {
+  max-width: 140rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.form {
+  //
+}
+
+.formContainer {
+  //
+}
+</style>
